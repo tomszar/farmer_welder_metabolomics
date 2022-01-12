a834b158b3a0bdba2ae210a267ec1ff1eec6eac9
tomszar <tomasgzarzar@gmail.com>
2022-01-12 16:39:50 -0500

    regression example, added grouping and color to pca plot

--- code/metabolite_summary_stats.py
+++ code/metabolite_summary_stats.py
@@ -2,19 +2,30 @@ import load
 import stats
 import figures
 import numpy as np
-import pandas as pd
+from scipy.stats import linregress
 
-metab = pd.read_csv('../data/metabolite_concentration.csv')
-concen = np.log2(metab.iloc[:, 4:] + 0.01)
+dat = load.load_complete_data()
+concen = np.log2(dat.iloc[:, 21:] + 0.01)
+groups = np.array(dat['research_subject'])
 
 # Violin plot
 figures.concentration_violinplot(concen)
 
 # PCA
 pca_scores = stats.generate_PCA(concen)
-figures.plot_pca_scores(pca_scores)
+figures.plot_pca_scores(pca_scores,
+                        groups=groups)
+figures.plot_pca_scores(pca_scores,
+                        continuous=dat['age'],
+                        filename='metabolites_pca_age')
+
+# Regression
+welders = dat.query('research_subject==1 or research_subject==2')
+x = welders['research_subject']
+y = np.log2(welders['Glycine'] + 0.01)
+b = ~np.isnan(x)
+slope, intercept, r, p, se = linregress(x[b], y[b])
 
 # Complete dataset
-dat = load.load_complete_data()
 dat.to_csv('../results/merged_data.csv',
            index=False)
