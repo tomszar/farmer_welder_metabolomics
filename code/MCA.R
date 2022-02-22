df7cf326a375cecdd46d4da808d27f92c1fb47d9
Tomas Gonzalez Zarzar <tomasgzarzar@gmail.com>
2022-02-22 14:22:50 -0500

    selected non-control exposures, updated main run

--- code/MCA.R
+++ code/MCA.R
@@ -3,7 +3,8 @@ library(FactoMineR)
 farmers <- read.csv("../results/farmers.csv")
 exposures <- farmers[9:14]
 exposures <- as.data.frame(lapply(exposures, factor))
-expo_mca <- MCA(exposures)
+variable_exposures <- exposures[exposures$mixed_or_applied == 1,]
+expo_mca <- MCA(variable_exposures[,-1], graph=FALSE)
 
 pdf(file="../results/MCA_exposure.pdf")
 plotellipses(expo_mca)
--- run_local.sh
+++ run_local.sh
@@ -3,4 +3,7 @@ echo "-----Running summary stats-----"
 cd code
 python metabolite_summary_stats.py
 
+echo "-----Running MCA-----"
+Rscript MCA.R
+
 echo "-----Done-----"
