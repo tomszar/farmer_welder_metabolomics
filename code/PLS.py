e270076d3e928409b18b5c6425ae673be0c776ae
tomszar <tomasgzarzar@gmail.com>
2022-02-23 16:14:40 -0500

    small test for PLS

--- code/PLS.py
+++ code/PLS.py
@@ -5,11 +5,26 @@ import pandas as pd
 
 from sklearn.preprocessing import OneHotEncoder, scale
 from sklearn.cross_decomposition import PLSRegression
+from sklearn.model_selection import LeavePOut
 
 farmers = load.load_data('farmers')
-exposures = farmers.iloc[:, 9:14]
+exposures = farmers.iloc[:, 8:14]
+exposure = exposures.iloc[:,0]
 concen = np.log2(farmers.iloc[:, 26:] + 0.01)
 concen = scale(concen)
+
+lpo = LeavePOut(10)
+lpo.get_n_splits(concen)
+
+i = 0
+for train_index, test_index in lpo.split(concen):
+    X_train, X_test = concen[train_index], concen[test_index]
+    y_train, y_test = exposure[train_index], exposure[test_index]
+    PLSDA = PLSRegression(n_components=1,
+                          scale=False)
+    PLSDA.fit(X_train, y_train)
+    y_pred = PLSDA.predict(X_test)
+    i = i + 1
 
 scores = stats.PLSR(concen, exposures, standardize_X=False)
 
