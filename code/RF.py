9e7b74c23114e185f6cc1e7d65d4b0a62e1785e4
tomszar <tomasgzarzar@gmail.com>
2022-02-25 12:35:52 -0500

    added grid search and evaluation of params

--- code/RF.py
+++ code/RF.py
@@ -1,26 +1,47 @@
 import load
 import numpy as np
-from sklearn.preprocessing import scale
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import LeavePOut
-from sklearn import metrics
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.metrics import classification_report
 
 farmers = load.load_data('farmers')
 exposures = farmers.iloc[:, 8:14]
-concen = np.log2(farmers.iloc[:, 26:] + 0.01)
-X = scale(concen)
-y = exposures.iloc[:, 0]
-lpo = LeavePOut(2)
+concen = farmers.iloc[:, 26:]
+X = np.array(concen)
+y = farmers['research_subject']
+subject_replace = {'Farmer': 1,
+                   'Farmer Control': 0}
+y = y.replace(subject_replace)
 
-accuracy = []
-for train_index, test_index in lpo.split(concen):
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-    clf = RandomForestClassifier(n_estimators=100,
-                                 min_samples_split=2,
-                                 random_state=10293,
-                                 verbose=0)
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
-    acc = metrics.accuracy_score(y_test, y_pred)
-    accuracy.append(acc)
+# Grid Search
+scoring = ('f1', 'recall', 'precision')
+SRF = RandomForestClassifier(class_weight='balanced_subsample',
+                             random_state=6739)
+param_grid = {
+    'n_estimators': [100, 150, 200],
+    'max_features': ['auto', 'sqrt', 'log2'],
+    'min_samples_split': [2, 3, 4],
+    'min_samples_leaf': [1, 2, 3],
+    'criterion': ['gini', 'entropy']
+}
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1652)
+CV_rfc = GridSearchCV(estimator=SRF,
+                      param_grid=param_grid,
+                      scoring=scoring,
+                      refit='f1',
+                      cv=5)
+CV_rfc.fit(X, y)
+CV_rfc.best_params_
+
+# Evaluate best parameters
+SRF = RandomForestClassifier(n_estimators=200,
+                             class_weight='balanced_subsample',
+                             random_state=6739)
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.1,
+                                                    random_state=1384,
+                                                    stratify=y)
+SRF.fit(X_train, y_train)
+y_pred = SRF.predict(X_test)
+print(classification_report(y_test, y_pred))
